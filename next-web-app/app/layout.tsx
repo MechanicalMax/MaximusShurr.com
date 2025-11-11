7c1682b464786a3e8701959cc082199e20eb61d0
Maximus Shurr <maxshurr@gmail.com>
2025-11-11 10:36:59 -0500

    feat(footer): Add navigation footer to bottom of site
    Includes my name, links around the site, and links to my social profiles

--- next-web-app/app/layout.tsx
+++ next-web-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,21 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <nav className="bg-gray-900">
+        <div className="container mx-auto px-4">
+          <div className="flex justify-between items-center py-4">
+            <div className="text-white font-bold text-2xl mr-4">Maximus Shurr</div>
+            <div className="flex flex-wrap gap-4 justify-end">
+              <Link href="/" className="text-white hover:text-gray-300">Home</Link>
+              <Link href="/book" className="text-white hover:text-gray-300">Book</Link>
+              <Link href="/resume" className="text-white hover:text-gray-300">Resume</Link>
+              <Link target="_blank" href="https://www.linkedin.com/m/in/maxshurr/" className="text-white hover:text-gray-300">LinkedIn</Link>
+              <Link target="_blank" href="https://github.com/MechanicalMax" className="text-white hover:text-gray-300">GitHub</Link>
+              <Link target="_blank" href="https://youtube.com/c/mechanicalmax" className="text-white hover:text-gray-300">YouTube</Link>
+            </div>
+          </div>
+        </div>
+      </nav>
     </html>
   );
 }
