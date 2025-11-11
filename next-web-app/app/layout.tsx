21b1bda4e45da4f3dd50564e4ec3544f97856b98
Maximus Shurr <maxshurr@gmail.com>
2025-11-11 11:01:10 -0500

    feat(footer): Add social icons
    Group footer nav links by text and icon links. Consistent animations for each

--- next-web-app/app/layout.tsx
+++ next-web-app/app/layout.tsx
@@ -34,13 +34,31 @@ export default function RootLayout({
         <div className="container mx-auto px-4">
           <div className="flex justify-between items-center py-4">
             <div className="text-white font-bold text-2xl mr-4">Maximus Shurr</div>
-            <div className="flex flex-wrap gap-4 justify-end">
-              <Link href="/" className="text-white hover:text-gray-300">Home</Link>
-              <Link href="/book" className="text-white hover:text-gray-300">Book</Link>
-              <Link href="/resume" className="text-white hover:text-gray-300">Resume</Link>
-              <Link target="_blank" href="https://www.linkedin.com/m/in/maxshurr/" className="text-white hover:text-gray-300">LinkedIn</Link>
-              <Link target="_blank" href="https://github.com/MechanicalMax" className="text-white hover:text-gray-300">GitHub</Link>
-              <Link target="_blank" href="https://youtube.com/c/mechanicalmax" className="text-white hover:text-gray-300">YouTube</Link>
+            <div className="flex flex-wrap justify-end gap-4 items-center">
+              {/* Text Links */}
+              <div className="flex flex-wrap gap-4 justify-end w-full sm:w-auto">
+                <Link href="/" className="text-white hover:text-gray-300">Home</Link>
+                <Link href="/book" className="text-white hover:text-gray-300">Book</Link>
+                <Link href="/resume" className="text-white hover:text-gray-300">Resume</Link>
+              </div>
+              {/* Icon Links */}
+              <div className="flex flex-wrap justify-end w-full sm:w-auto">
+                <Link target="_blank" href="https://www.linkedin.com/m/in/maxshurr/" className="text-white hover:text-gray-300 transition-colors duration-200 p-2 inline-flex items-center">
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                  </svg>
+                </Link>
+                <Link target="_blank" href="https://github.com/MechanicalMax" className="text-white hover:text-gray-300 transition-colors duration-200 p-2 inline-flex items-center">
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                    <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.7-2.782.605-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.462-1.11-1.462-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.268 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.027 2.747-1.027.546 1.377.202 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.14 20.21 22 16.44 22 12.017 22 6.484 17.522 2 12 2z" />
+                  </svg>
+                </Link>
+                <Link target="_blank" href="https://youtube.com/c/mechanicalmax" className="text-white hover:text-gray-300 transition-colors duration-200 p-2 inline-flex items-center">
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                    <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
+                  </svg>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
